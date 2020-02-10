54095f6b2b342d433f1419b99be006f63d0e681f
Amador Pahim <apahim@redhat.com>
2020-02-10 14:12:00 +0000

    Small fixes to build_deploy.sh
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,10 +2,10 @@
 
 DOCKER_CONF="${PWD}/.docker"
 mkdir -p "${DOCKER_CONF}"
-docker --config="${DOCKER_CONF}" login -u="${QUAY_USER}" -p="{$QUAY_TOKEN}" quay.io
+docker --config="${DOCKER_CONF}" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 
 IMAGE_NAME="quay.io/app-sre/github-mirror"
-IMAGE_TAG=$(shell git rev-parse --short=7 HEAD)
+IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 
 docker build -t "${IMAGE_NAME}:latest" .
 docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${IMAGE_TAG}"
