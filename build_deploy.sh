4cab78f51b5493bd41686380c431e9d596cb131b
Amador Pahim <apahim@redhat.com>
2020-02-13 16:16:18 +0000

    Moving the config generation to after the build
    So we avoid trying to copy the config into the image.
    
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DOCKER_CONF="${PWD}/.docker"
-mkdir -p "${DOCKER_CONF}"
-docker --config="${DOCKER_CONF}" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
-
 IMAGE_NAME="quay.io/app-sre/github-mirror"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 
 docker build -t "${IMAGE_NAME}:latest" .
 docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${IMAGE_TAG}"
+
+DOCKER_CONF="${PWD}/.docker"
+mkdir -p "${DOCKER_CONF}"
+docker --config="${DOCKER_CONF}" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 
 docker --config="${DOCKER_CONF}" push "${IMAGE_NAME}:latest"
 docker --config="${DOCKER_CONF}" push "${IMAGE_NAME}:${IMAGE_TAG}"
