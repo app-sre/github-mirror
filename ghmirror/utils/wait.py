a2f82eb005690a868deffe2776ccbf7c40abb637
Amador Pahim <apahim@redhat.com>
2020-07-08 17:39:21 +0100

    Fix race condition in tests
    The thread checking the mirror state could go offline before we make the
    first call to build up the cache.
    
    This patch adds more control to when the mirror goes offline/online,
    mitigating the race condition.
    
    Signed-off-by: Amador Pahim <apahim@redhat.com>

---
+++ ghmirror/utils/wait.py
@@ -0,0 +1,31 @@
+"""
+Functions to help waiting for a given state
+"""
+import time
+
+
+def wait_for(func, timeout, first=0.0, step=1.0, args=None, kwargs=None):
+    """
+    Wait until func() evaluates to True.
+
+    If func() evaluates to True before timeout expires, return the
+    value of func(). Otherwise return None.
+
+    :param timeout: Timeout in seconds
+    :param first: Time to sleep before first attempt
+    :param step: Time to sleep between attempts in seconds
+    :param args: Positional arguments to func
+    :param kwargs: Keyword arguments to func
+    """
+    if args is None:
+        args = []
+    if kwargs is None:
+        kwargs = {}
+    start_time = time.monotonic()
+    end_time = start_time + timeout
+    time.sleep(first)
+    while time.monotonic() < end_time:
+        result = func(*args, **kwargs)
+        if result:
+            return result
+        time.sleep(step)
