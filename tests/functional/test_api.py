e0ace657e3e228437b36b2a5f5feb0d9481c3363
Andreu Gallofre <agallofr@redhat.com>
2021-08-04 10:14:55 +0200

    Build URL based on params to mantain current cahce keys
    In #57 we removed the rebuilding of the URL as we changed how to pass
    parameters to the conditional requests, but as we are using the url as
    part of the cache key we need the url to keep the cache of the pages

--- tests/functional/test_api.py
+++ tests/functional/test_api.py
@@ -173,7 +173,7 @@ def test_mirror_last_modified(mock_get, client):
 def test_mirror_upstream_call(mocked_request, client):
     client.get('/user/repos?page=2',
                headers={'Authorization': 'foo'})
-    expected_url = 'https://api.github.com/user/repos'
+    expected_url = 'https://api.github.com/user/repos?page=2'
     mocked_request.assert_called_with(method='GET',
                                       headers={'Authorization': 'foo'},
                                       url=expected_url,
