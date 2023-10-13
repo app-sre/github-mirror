247daa9395c26a0f9092dcaf4e5c729c4d3bd09a
Jan-Hendrik Boll <jboll@redhat.com>
2023-10-13 14:23:07 +0200

    Revert "Use quay appsre image"

--- pr_check.sh
+++ pr_check.sh
@@ -2,9 +2,5 @@
 
 IMAGE_TEST=ghmirror-test
 
-DOCKER_CONF="${PWD}/.docker"
-mkdir -p "${DOCKER_CONF}"
-docker --config="${DOCKER_CONF}" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
-
 docker build -t ${IMAGE_TEST} -f Dockerfile.test .
 docker run --rm ${IMAGE_TEST}
