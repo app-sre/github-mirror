ed2b78832ae46bfe47236c276a48bd8842c5d49c
Hemslo Wang <dwan@redhat.com>
2024-07-02 17:47:51 +1000

    Upgrade requests to 2.32.3 (#135)
    Signed-off-by: Di Wang <dwan@redhat.com>

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     ],
     install_requires=[
         "Flask~=3.0.3",
-        "requests~=2.32",
+        "requests~=2.32.3",
         "prometheus_client~=0.20",
         "gunicorn==22.0.0",
         "redis~=5.0.4",
